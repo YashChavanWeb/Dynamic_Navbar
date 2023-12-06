adf733f462513562b0f4839033ae5f11dc1ef20f
YashChavanWeb <yashchavan4628@gmail.com>
2023-12-06 15:03:33 +0530

    Done with the first project

---
+++ script.js
@@ -0,0 +1,19 @@
+// Add an event listener to the window object for the 'scroll' event
+window.addEventListener("scroll", function () {
+    // Inside the event listener function
+
+    // header Selection:
+    // This is a reference to the <header> DOM element on the web page.
+    const header = document.querySelector("header");
+
+    // classList:
+    // It's a property of DOM elements for manipulating classes.
+    // toggle Method:
+    // Toggles/updates the presence of a class based on a boolean condition.
+    header.classList.toggle("navbar-scrolled", window.scrollY > 0);
+
+    // window.scrollY:
+    // Represents the current vertical scroll position.
+    // If greater than 0, the user has scrolled down the page.
+
+});
